70a92f9fac0e196b784e01377a9950f2d60ea305
dulkappa <dulkappa@gmail.com>
2014-08-17 10:27:20 +0900

    Modify admin.py (indicate records)

--- cms/admin.py
+++ cms/admin.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from cms.models import Collection, Item
 
-# Register your models here.
+# admin.site.register(Collection)
+# admin.site.register(Item)
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'saummary',)  # 一覧に出したい項目
+    list_display_links = ('id', 'name',)  # 修正リンクでクリックできる項目
+admin.site.register(Collection, CollectionAdmin)
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    list_display_links = ('id', 'name',)
+admin.site.register(Item, ItemAdmin)
