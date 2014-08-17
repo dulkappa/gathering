88cdfdf6222ae3f9dd9b1bdb4480260d0f739f58
dulkappa <dulkappa@gmail.com>
2014-08-17 10:13:58 +0900

    Add models.py

--- cms/models.py
+++ cms/models.py
@@ -1,3 +1,18 @@
+# -*- coding:utf-8 -*-
 from django.db import models
 
-# Create your models here.
+class Collection(models.Model):
+  '''コレクション'''
+  name = models.CharField(u'コレクション名', max_length=255)
+  saummary = models.TextField(u'概要', blank=True)
+
+  def __unicode__(self):
+    return self.name
+
+class Item(models.Model):
+  '''項目'''
+  collection = models.ForeignKey(Collection, verbose_name=u'コレクション', related_name='items')
+  name = models.CharField(u'項目名', max_length=255)
+
+  def __unicode__(self):
+    return self.name
