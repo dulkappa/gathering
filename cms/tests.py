1969ba21661c4f55666451bf786606b960e75ecd
dulkappa <dulkappa@gmail.com>
2014-08-19 06:27:27 +0900

    first test with Travis CI

--- cms/tests.py
+++ cms/tests.py
@@ -1,3 +1,10 @@
+# -*- coding:utf-8 -*-
 from django.test import TestCase
 
-# Create your tests here.
+from cms.models import Collection
+
+class CollectionTests(TestCase):
+
+  def test_is_collection_added(self):
+    collection = Collection(name='test', summary='a sample collection for test.')
+    self.assertEqual(len(collection.name), 4)
